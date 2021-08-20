9c1b7744798cfec6aa69b5db8a286f6b0331036c
220alexandre <conquer.aliance@gmail.com>
2021-08-19 23:35:54 -0300

    precos

--- public/Constantes/vip.php
+++ public/Constantes/vip.php
@@ -11,73 +11,73 @@ define('PRECO_DOBRAO_COUP_DE_BURST',                    600);
 define('PRECO_GOLD_COUP_DE_BURST_INSTANTANEO',          20);
 define('PRECO_DOBRAO_COUP_DE_BURST_INSTANTANEO',        30);
 
-define('PRECO_GOLD_TATICAS',                            500);
-define('PRECO_DOBRAO_TATICAS',                          1200);
+define('PRECO_GOLD_TATICAS',                            250);
+define('PRECO_DOBRAO_TATICAS',                          600);
 
 define('PRECO_GOLD_CAMUFLAGEM',                         500);
 define('PRECO_DOBRAO_CAMUFLAGEM',                       1200);
 
-define('PRECO_GOLD_RENOMEAR_TRIPULACAO',                500);
-define('PRECO_DOBRAO_RENOMEAR_TRIPULACAO',              1200);
+define('PRECO_GOLD_RENOMEAR_TRIPULACAO',                250);
+define('PRECO_DOBRAO_RENOMEAR_TRIPULACAO',              600);
 
 define('PRECO_GOLD_TROCAR_FACCAO',                      500);
 define('PRECO_DOBRAO_TROCAR_FACCAO',                    1200);
 
-define('PRECO_GOLD_FINALIZAR_TRATAMENTO_HOSPITAL',      150);
-define('PRECO_DOBRAO_FINALIZAR_TRATAMENTO_HOSPITAL',    360);
+define('PRECO_GOLD_FINALIZAR_TRATAMENTO_HOSPITAL',      50);
+define('PRECO_DOBRAO_FINALIZAR_TRATAMENTO_HOSPITAL',    120);
 
-define('PRECO_GOLD_ISCA_10',                            100);
-define('PRECO_DOBRAO_ISCA_10',                          240);
+define('PRECO_GOLD_ISCA_10',                            10);
+define('PRECO_DOBRAO_ISCA_10',                          24);
 
-define('PRECO_GOLD_ISCA_130',                           300);
-define('PRECO_DOBRAO_ISCA_130',                         720);
+define('PRECO_GOLD_ISCA_130',                           30);
+define('PRECO_DOBRAO_ISCA_130',                         72);
 
-define('PRECO_GOLD_RESET_AKUMA',                        400); // 100
-define('PRECO_DOBRAO_RESET_AKUMA',                      960);
+define('PRECO_GOLD_RESET_AKUMA',                        100); // 100
+define('PRECO_DOBRAO_RESET_AKUMA',                      250);
 
-define('PRECO_GOLD_REDEFINE_AKUMA',                     300); // 10
-define('PRECO_DOBRAO_REDEFINE_AKUMA',                   720);
+define('PRECO_GOLD_REDEFINE_AKUMA',                     30); // 10
+define('PRECO_DOBRAO_REDEFINE_AKUMA',                   70);
 
-define('PRECO_GOLD_RESET_ATRIBUTOS',                    25); // 5
-define('PRECO_DOBRAO_RESET_ATRIBUTOS',                  60);
+define('PRECO_GOLD_RESET_ATRIBUTOS',                    10); // 5
+define('PRECO_DOBRAO_RESET_ATRIBUTOS',                  15);
 
-define('PRECO_GOLD_RESET_CLASSE',                       100); // 15
-define('PRECO_DOBRAO_RESET_CLASSE',                     240);
+define('PRECO_GOLD_RESET_CLASSE',                       15); // 15
+define('PRECO_DOBRAO_RESET_CLASSE',                     36);
 
-define('PRECO_GOLD_RESET_DISPOSICAO',                   100);
-define('PRECO_DOBRAO_RESET_DISPOSICAO',                 240);
+define('PRECO_GOLD_RESET_DISPOSICAO',                   15);
+define('PRECO_DOBRAO_RESET_DISPOSICAO',                 36);
 
-define('PRECO_GOLD_RESET_HAKI',                         100); // 15
-define('PRECO_DOBRAO_RESET_HAKI',                       240);
+define('PRECO_GOLD_RESET_HAKI',                         15); // 15
+define('PRECO_DOBRAO_RESET_HAKI',                       36);
 
 define('PRECO_GOLD_TREINAR_HAKI',                       1);
 define('PRECO_DOBRAO_TREINAR_HAKI',                     2);
 
-define('PRECO_GOLD_RESET_NOME_PERSONAGEM',              250);
-define('PRECO_DOBRAO_RESET_NOME_PERSONAGEM',            600);
+define('PRECO_GOLD_RESET_NOME_PERSONAGEM',              50);
+define('PRECO_DOBRAO_RESET_NOME_PERSONAGEM',            120);
 
-define('PRECO_GOLD_RESET_PROFISSAO',                    100); // 15
-define('PRECO_DOBRAO_RESET_PROFISSAO',                  240);
+define('PRECO_GOLD_RESET_PROFISSAO',                    15); // 15
+define('PRECO_DOBRAO_RESET_PROFISSAO',                  36);
 
-define('PRECO_GOLD_VIAJA_RETORNO',                      200);
-define('PRECO_DOBRAO_VIAJA_RETORNO',                    480);
+define('PRECO_GOLD_VIAJA_RETORNO',                      50);
+define('PRECO_DOBRAO_VIAJA_RETORNO',                    120);
 
-define('PRECO_MODIFICADOR_RECRUTAR_LVL_ALTO',           15);
-define('PRECO_MODIFICADOR_DOBRAO_RECRUTAR_LVL_ALTO',    36);
+define('PRECO_MODIFICADOR_RECRUTAR_LVL_ALTO',           3);
+define('PRECO_MODIFICADOR_DOBRAO_RECRUTAR_LVL_ALTO',    7);
 
 define('PRECO_GOLD_RECUPERAR_OFENSIVA_MESTRE_HAKI',     1);
 define('PRECO_DOBRAO_RECUPERAR_OFENSIVA_MESTRE_HAKI',   2);
 
-define('PRECO_GOLD_FERREIRO_VIP',                       50);
-define('PRECO_DOBRAO_FERREIRO_VIP',                     120);
+define('PRECO_GOLD_FERREIRO_VIP',                       10);
+define('PRECO_DOBRAO_FERREIRO_VIP',                     24);
 
 define('PRECO_BERRIES_TRANSPORTE_ILHA_RETORNO',         2000000);
 
-define('PRECO_GOLD_TROCAR_PERSONAGEM',                  150);
-define('PRECO_DOBRAO_TROCAR_PERSONAGEM',                360);
+define('PRECO_GOLD_TROCAR_PERSONAGEM',                  50);
+define('PRECO_DOBRAO_TROCAR_PERSONAGEM',                120);
 
-define('PRECO_GOLD_TROCAR_BANDEIRA',                    250);
-define('PRECO_DOBRAO_TROCAR_BANDEIRA',                  600);
+define('PRECO_GOLD_TROCAR_BANDEIRA',                    50);
+define('PRECO_DOBRAO_TROCAR_BANDEIRA',                  120);
 
 define('PRECO_GOLD_RESETAR_INFLUENCIA',                 200);
 define('PRECO_DOBRAO_RESETAR_INFLUENCIA',               480);
@@ -85,7 +85,7 @@ define('PRECO_DOBRAO_RESETAR_INFLUENCIA',               480);
 define('PRECO_GOLD_USAR_FORMACOES',                     100);
 define('PRECO_DOBRAO_USAR_FORMACOES',                   240);
 
-define('PRECO_GOLD_CUSTOMIZAR_SKILL',                   75);
-define('PRECO_DOBRAO_CUSTOMIZAR_SKILL',                 180);
+define('PRECO_GOLD_CUSTOMIZAR_SKILL',                   10);
+define('PRECO_DOBRAO_CUSTOMIZAR_SKILL',                 24);
 
-define('PRECO_GOLD_SELO_EXP',                           75);
+define('PRECO_GOLD_SELO_EXP',                           10);
