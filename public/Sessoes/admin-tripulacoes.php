c87daeed893ee9e14712f6c69a0a0f581b59d2fc
220alexandre <conquer.aliance@gmail.com>
2021-08-28 23:16:04 -0300

    remove

--- public/Sessoes/admin-tripulacoes.php
+++ public/Sessoes/admin-tripulacoes.php
@@ -17,13 +17,11 @@ if (isset($_GET["pagina"]) && validate_number($_GET["pagina"])) {
         <tr>
             <th width="50" class="text-center">Id</th>
             <th width="10" class="text-center">Conta Id</th>
-            <th width="10" class="text-center">E-mail</th>
             <th width="2">Tripulaçao</th>
             <th width="2">Berries</th>
             <th width="2">ADM</th>
             <th width="2">Vitorias</th>
-            <th width="2">Reputaçao era</th>
-            <th width="2">Reputaçao mes</th>
+
 
         </tr>
         </thead>
@@ -32,13 +30,11 @@ if (isset($_GET["pagina"]) && validate_number($_GET["pagina"])) {
             <tr>
             <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['id'] ?></td>
             <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['conta_id'] ?></td>
-            <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['email'] ?></td>
             <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['tripulacao'] ?></td>
             <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['berries'] ?></td>
             <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['adm'] ?></td>
             <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['vitorias'] ?></td>
-            <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['reputacao'] ?></td>
-            <td style="vertical-align: middle;" class="text-center"><?php echo $tripulacao['reputacao_mensal'] ?></td>
+
             </tr>
         <?php endforeach; ?>
         </tbody>
