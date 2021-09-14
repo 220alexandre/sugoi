59135e31f839c56eee8d505d8936ef39a504add7
220alexandre <conquer.aliance@gmail.com>
2021-09-13 23:36:49 -0300

    teste

--- public/Sessoes/admin-reagents.php
+++ public/Sessoes/admin-reagents.php
@@ -22,9 +22,11 @@
                 <td style="vertical-align: middle;" class="text-center"><?php echo $equipamento['cod_reagent'] ?></td>
                 <td style="vertical-align: middle;">
                     <span class="equipamentos_casse_<?= $equipamento["categoria"] ?>">
+                    
                         <?php echo $equipamento['nome'] ?>
                     </span><br />
                     <small><?php echo $equipamento['descricao'] ?></small>
+                    <td><img src="Imagens/Itens/<?= $equipamento["img"] ?>.png"/></td>
                 </td>
         <?php endforeach; ?>
         </tbody>
